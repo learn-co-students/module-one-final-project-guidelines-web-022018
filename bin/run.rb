9be8de877656188c4989eabec0e7a00a4c842398
dannyb21892 <dannyb21892@verizon.net>
2018-03-06 11:32:16 -0500

    testing

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,10 @@
-require_relative 'config/environment'
+require_relative '../config/environment'
 
+#url = "http://gateway.marvel.com/v1/public/characters"
+#url = "https://gateway.marvel.com:443/v1/public/characters/1009610/events?name=Age%20of%20Ultron"
+url = get_api_URL(welcome)
+
+response = RestClient.get(get_api_URL(url))
+json = JSON.parse(response)
+binding.pry
+"hi"
