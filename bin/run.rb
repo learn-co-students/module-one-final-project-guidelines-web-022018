cb62bf70e013cb19af8da52724f049624034ba33
bafab <bafa@tutanota.de>
2018-03-07 15:17:25 -0500

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,5 @@
-require_relative 'config/environment'
+require_relative '../config/environment.rb'
 
+puts "Welcome to Rock, Paper, Scissors"
+game = Game.new
+game.begin
--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,79 @@
 class Game
+  @@all = []
 
-	def new_move
+  def self.all
+    @@all
+  end
+
+  def begin
+    puts "Are you ready to play? (yes/no)"
+  	response = gets.chomp.downcase
+  	if response == "yes"
+  		puts "Game is on!"
+  		"Game is on!"
+  		login
+  	elsif response == "no"
+  		puts "Fine, call your mommy."
+  		"Fine, call your mommy."
+  	else
+  		puts "Invalid command. Please try again, loser." # this works!
+			welcome  # this works!
+  	end
+  end
+
+  def login
+    puts "Enter your username: "
+    username = gets.chomp.downcase
+    if @@all.include?(username)
+      puts "You already exist here. You are now logged in."
+      "You already exist here. You are now logged in."
+    else @@all << username
+      #username = User.new(username)
+      puts "You don't exist yet. User created."
+      "You don't exist yet. User created."
+    end
+    new_move
+  end
+
+  def new_move
 		puts "Enter your move: "
-		move = gets.chomp.downcase
-		if move != "rock" && move != "paper" && move != "scissors"
-			return "Invalid command. Learn the game, idiot!"
-		end
+		@move = gets.chomp.downcase
+    @computer = ['rock', 'paper', 'scissors'].sample
+    if ['rock', 'paper', 'scissors'].include?(@move)
+      puts @computer
+      if @move == @computer
+        puts "Draw. Go again."
+        new_move
+      else
+        results
+      end
+    else
+      puts "Invalid command. Learn the game, idiot!"
+      new_move
+    end
 	end
 
-	
-
-	
+  def results
+    if (@move == "rock" && @computer == "scissors") ||
+        (@move == "paper" && @computer == "rock") ||
+        (@move == "scissors" && @computer == "paper")
+        puts "You win. \n Play again? (yes/no)"
+        response = gets.chomp.downcase
+        if response == "yes"
+          new_move
+        else
+          exit
+        end
+    elsif (@move == "scissors" && @computer == "rock") ||
+        (@move == "paper" && @computer == "scissors") ||
+        (@move == "rock" && @computer == "paper")
+        puts "You lost, as expected. Get a life, loser! \n Play again? (yes/no)"
+        response = gets.chomp.downcase
+        if response == "yes"
+          new_move
+        else
+          exit
+        end
+    end
+  end
 end
